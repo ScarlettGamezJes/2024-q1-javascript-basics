168de39bc288e698be9a371b5bd7f664fc61f378
Jesse Silva <joshuansilva@gmail.com>
2024-09-13 15:42:51 -0600

    rps done (Few errors)

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,49 +1,50 @@
 //@ts-check
 
-add(1, 2)
+add(1, 2);
 
-// let firstNAME = "John";
+let firstNAME = "John";
+
 // add default parameter view
 // @ts-ignore
-let sayHi = function(firstNAME,= "Captin No Name", age = 100000) {
-    let greeting = `Hello! My name is ${firstNAME} and I am ${age} years old.`;
-    // "Hello! My name is " + firstNAME " and I am " + age + " years old.";
-    console.log(greeting);
-    };
+let sayHi = function (firstNAME = "Captin No Name", age = 100000) {
+	let greeting = `Hello! My name is ${firstNAME} and I am ${age} years old.`;
+	// "Hello! My name is " + firstNAME " and I am " + age + " years old.";
+	console.log(greeting);
+};
 //call with default params
-    sayHi();
-    sayHi("Jake");
-    //call with default age
+sayHi();
+sayHi("Jake");
+//call with default age
 sayHi("Robert", 53);
-sayHi(firstNAME, 19)
+sayHi(firstNAME, 19);
 
 // console.log(firstNAME);
 
 // this function is hoisted to the global scope
 // to the global scope
 function add(a, b) {
-let answer = a + b;
-    return answer;
-};
+	let answer = a + b;
+	return answer;
+}
 let additionAnswer = add(100, 5000);
 add(5000, 9873498739873);
 
 console.log(additionAnswer, add(5000, 9873498739873));
 let runMystuff = (x, y, f) => {
-    let answer = f(x, y);
-    return answer;
+	let answer = f(x, y);
+	return answer;
 };
 
-runMystuff("WillLuke", 28, sayHi)
+runMystuff("WillLuke", 28, sayHi);
 
 let person = {
-    isWearingBarneyShirt: false,
-    isTotallyAwesome: false
+	isWearingBarneyShirt: false,
+	isTotallyAwesome: false,
 };
 
 function wearBarneyShirt(person) {
-    person.isWearingBarneyShirt = true;
-    person.isTotallyAwesome
+	person.isWearingBarneyShirt = true;
+	person.isTotallyAwesome;
 }
 
 console.log(person);
---
+++ scripts/rps-games.js
@@ -0,0 +1,101 @@
+//@ts-check
+
+/** @type { HTMLElement } */
+//@ts-ignore We Know game result header is not null
+let playerControls = document.getElementById("player-controls");
+
+//game-results
+/** @type { HTMLElement } */
+//@ts-ignore We Know game result header is not null
+let gameResultHeader = document.getElementById("game-results");
+
+/** @type { HTMLElement } */
+//@ts-ignore We Know game result header is not null
+let gameResultSection = document.getElementById("game-restart");
+
+gameResultHeader.innerText = "Hey there!!";
+
+let isGameOver = false;
+
+let weapons = [
+	{
+		type: "Rock",
+		beats: "Scissors",
+	},
+	{
+		type: "Paper",
+		beats: "Rock",
+	},
+	{
+		type: "Scissors",
+		beats: "Paper",
+	},
+];
+
+function pickRandomWeapon(weapons) {
+	let randy = Math.floor(Math.random() * weapons.length);
+	return weapons[randy];
+}
+
+function determineOutcome(playerWeapon, computerWeapon) {
+	if (playerWeapon.type === computerWeapon.type) {
+		return "it's a tie! Try again";
+	}
+
+	isGameOver = true;
+	showRestart();
+
+	if (playerWeapon.beats === computerWeapon.weapon) {
+		return `Player wins! ${playerWeapon.type}`;
+	}
+
+		return `Computer wins! ${computerWeapon.type}`;
+	}
+
+
+function playerControlHandler(e) {
+	//if (isGameOver) {
+	//	return;
+	//}
+	if (isGameOver) {
+		return;
+	}
+
+	let weaponName = e.target.innerText;
+	let playerWeapon = weapons.find((w) => w.type === weaponName);
+
+	if (!playerWeapon) {
+		console.log("ERROR! Player weopon undefined");
+		return;
+	}
+
+	let computerWeapon = pickRandomWeapon(weapons);
+
+	let result = determineOutcome(playerWeapon, computerWeapon);
+
+	gameResultHeader.innerText = result;
+
+	console.log(result);
+
+}
+
+function gameResultHandler(e){
+if (e.target.id === "btn-restart") {
+	isGameOver = false;
+	showPlayerControls();
+	gameResultHeader.innerText = "";
+}
+}
+
+function showRestart() {
+    gameRestartSection.style.display = "initial";
+	playerControls.style.display = "none";
+}
+
+function showPlayerControls() {
+	gameRestartSection.style.display ="none";
+playerControlHandler.style.display = "initial";
+}
+playerControls?.addEventListener("click", playerControlHandler);
+
+gameRestartSection.addEventListener("click", gameResultHandler)
